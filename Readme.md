e2aaa56434992909e5e3711985eaf59c248824ac
Evandro dos Santos <evandro.sl@aluno.ifsc.edu.br>
2020-12-05 18:54:45 +0000

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,66 @@
+**Trabalho 2**
+Vencimento
+Tempo restante: 4 dias 8 horas
+Simulador de memória cache
+
+Participantes por grupo: 2
+
+Politica de Mapeamento Direto:
+1 -  Direto
+2 - Associativo
+3 - Associativo por conjunto
+
+-- Politica de escrita
+1. Write - Through
+2. Write - Back
+
+-- Politica de substituição
+1 - LRU
+2 - LFU
+** 3 - FIFO **
+4 - ALEATÓRIO
+
+//Implementação
+ Pode ser implementado em qualquer linguagem, e deve seguir o padrão das estruturas do código em C abaixo, respeitando todos os tamanhos dados. 
+
+** MemCache cache[16]; //tamanho de quadros ou conjuntos **
+** MemPrincipal principal[2048]; //número de células **
+
+
+//Opções de Menu
+
+1- Ler Memoria
+2- Escrever memoria
+3- Estatísticas
+
+As estatísticas abaixo devem ser implementadas:
+
+3.1 Numero de Acessos
+3.2 Numero de Acertos
+3.3 Numero de Faltas
+3.4 Numero de Leituras
+3.5 Numero de Escritas
+3.6 Numero de Acertos na Leitura
+3.7 Numero de Acertos na Escrita
+3.8 Numero de Faltas na Leitura
+3.9 Numero de Faltas na Escrita
+
+
+//Pontuação
+Em caso da politica direta:
+Politica de Mapeamento: 5 pts
+ Politica de escrita: 2 pts
+Estatísticas: 3 pts
+Senão:
+Politica de Mapeamento: 5 pts
+Politica de troca: 1 pts
+Politica de escrita: 2 pts
+Estatísticas: 2 pts
+
+//Observações:
+
+O tamanho do quadro deve ser definido pelo grupo.
+Os dados de endereço e rótulo devem ser exibidos em binário.
+O conteúdo de cada célula é um char, que pode ser preenchido aleatoriamente na memória principal.
+Caso seja necessário adição de variáveis ou modificação no tamanho dos atributos nas estruturas, deve ser feito. Mas sempre respeitando o tamanho da cache, da memória principal e das células.
+Duplas serão definidas e sorteados os temas durante a conferência.
---
+++ main.c
@@ -0,0 +1,328 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define TAM 16
+#define TELA system("clear");
+
+// Structs
+typedef struct celula
+{
+    char endereco[11];
+    char conteudo;
+    int validar;
+	
+} t_celula;
+
+typedef struct blocoMemoria
+{
+    int bloco;
+    t_celula celula_bloco[4];
+} t_bloco;
+
+typedef struct linha_quadro
+{
+    int alterado;
+    int contador_de_acesso;
+    t_bloco bloco_cache;
+} t_quadro;
+
+typedef struct ESTFILA
+{
+    int INI, FIM;
+    t_quadro linha_quadro[TAM];
+} t_Fila;
+
+
+
+// inicia memoria principal
+void insereConteudo(int num, t_celula *celula_memo_principal)
+{
+
+    char binario[11];
+    char binarioCorreto[11];
+
+    int i, j = 0, k, cont = 0;
+
+    // limpa lixo
+    for (i = 0; i < 11; i++)
+    {
+      binario[i] = 48;
+		  binarioCorreto[i] = 48;
+    }
+
+    cont = num;
+
+    i = 0;
+    // atribui binario inverso
+    while (num > 0)
+    {
+				if(num % 2 == 0)
+				{
+					binario[i] = 48;
+				}
+				else
+				{
+					binario[i] = 49;
+				}
+        i++;
+        num = num / 2;
+    }
+    // define char aleatório
+    (*celula_memo_principal).conteudo = (rand() % 26) + 97;
+	
+    // inverte ordem
+    for (i = 10; i >= 0; i--)
+    {
+        (*celula_memo_principal).endereco[j] = binario[i];
+				j++;
+    }
+}
+
+// Visualização 
+void mostraMemoriaPricipal(t_celula MemPrincipal[])
+{
+		TELA;
+    printf("Mostrando MemPrincipal: \n");
+
+    printf("Endereco");
+    printf("\t\tconteudo");
+    for (int j = 0; j < 2048; j++)
+    {
+        printf("\n");
+				printf("%s", MemPrincipal[j].endereco);
+        printf("\t\t %c", MemPrincipal[j].conteudo);
+    }
+    getchar();
+    getchar();
+}
+
+void mostraMemoriaCache(t_quadro MemCache[])
+{
+    TELA;
+    printf("\nMostrando MemCache: \n");
+    printf("Alterado");
+    printf("\t Acessos");
+    printf("\tConteudo\t");
+    printf("\tEndereco de inicio do Bloco");
+    for (int mostratodoCache = 0; mostratodoCache < 64;)
+    {
+        for (int i = 0; i < 16; i++)
+        {
+            printf("\n");
+            printf("%d ", MemCache[i].alterado);
+            printf("\t\t\t\t%d \t", MemCache[i].contador_de_acesso);
+            for (int j = 0; j < 4; j++)
+            {
+
+                printf("\t %c", MemCache[i].bloco_cache.celula_bloco[j].conteudo);
+
+                mostratodoCache++;
+            }
+            printf("\t");
+            for (int h = 0; h < 11; h++)
+            {
+                printf("%d", MemCache[i].bloco_cache.celula_bloco[0].endereco[h]);
+            }
+        }
+    }
+
+    getchar();
+    getchar();
+}
+
+// Funções da fila
+t_Fila CriarFila(void)
+{
+    t_Fila F;
+    F.INI = -1;
+    F.FIM = -1;
+    return F;
+}
+
+int FilaVazia(struct ESTFILA F)
+{
+    if (F.INI == -1 && F.FIM == -1)
+        return 1;
+    else
+        return 0;
+}
+
+t_Fila QUEUE(t_quadro linha_do_quadro, t_Fila F)
+{
+    if (FilaVazia(F))
+    {
+        F.INI++;
+        F.FIM++;
+    }
+    else
+    {
+        F.FIM++;
+        if (F.FIM == TAM && F.INI != 0)
+            F.FIM = 0;
+    }
+    F.linha_quadro[F.FIM] = linha_do_quadro;
+    return F;
+}
+
+t_quadro
+DEQUEUE(t_Fila F)
+{
+    t_quadro Valor;
+    Valor = F.linha_quadro[F.INI];
+    if (F.INI == F.FIM)
+    {
+        F.INI = -1;
+        F.FIM = -1;
+    }
+    else
+    {
+        F.INI++;
+        if (F.INI == TAM && F.FIM >= 0)
+            F.INI = 0;
+    }
+    return Valor;
+}
+
+
+// Funções Estatísticas
+int miss = 0;
+int fail = 0;
+
+void conta_acertos()
+{
+    miss++;
+}
+
+void conta_erros()
+{
+    fail++;
+}
+
+
+void lerMemoria(t_Fila FilaCache, t_bloco Blocos[], t_celula Celulas[])
+{
+  int achou, cont;
+  achou = cont = 0;
+  int num = 0;
+  char endereco[11];
+  fflush(stdin);
+  printf("Digite o Endereco a ser buscado: ");
+  scanf("%s", endereco);
+  printf("\nEndereço digitado: %s", endereco);
+        
+
+  while (achou != 1 || cont < 2048)
+  {
+    for(int hh = 0; hh < 11; hh++)
+    {
+      if(endereco[hh] == Celulas[cont].endereco[hh])
+      {
+        num++;
+      }
+      if(num == 11)
+      {
+        achou = 1;
+        printf("deu boa\n");
+      }
+      else
+        {
+          num = 0;
+        }
+    }
+		cont++;
+		}
+achou ? printf("\n Achou\n"):printf("\n Nao achou\n"); 
+  getchar();
+  getchar();
+}
+
+
+int main(void)
+{
+
+    t_Fila Fila = CriarFila();
+
+    t_celula MemPrincipal[2048];
+    t_quadro MemCache[16];
+    t_bloco Bloco[512];
+
+    // Iniciar memória Principal
+    int contador_bloco = 0;
+    for (int j = 0; j < 2048;)
+    {
+        // inserindo conteudo em quatro células de memoria
+        for (int i = 0; i < 4; i++)
+        {
+            insereConteudo(j, &MemPrincipal[j]);
+
+            Bloco[contador_bloco].bloco = contador_bloco;
+            Bloco[contador_bloco].celula_bloco[i] = MemPrincipal[j];
+            MemPrincipal[j].validar = 0;
+				
+            j++;
+        }
+        // mudando contador do bloco, mas continua inserindo conteudo
+        contador_bloco++;
+    }
+
+    // Iniciar Cache
+    int quadro = 0;
+    for (int i = 0; i < 16; i++)
+    {
+        MemCache[i].alterado = 0;
+        MemCache[i].contador_de_acesso = 0;
+        
+        for (quadro = 0; quadro < 4; quadro++)
+        {
+            MemCache[i].bloco_cache =
+                Bloco[i];
+            
+        }
+        Fila = QUEUE(MemCache[i], Fila);
+    }
+
+    // menu
+    int op;
+
+    while (1)
+    {
+        TELA;
+        printf("\nMenu");
+        printf("\n\n 1- Ler Memoria");
+        printf("\n 2- Escrever memoria ");
+        printf("\n 3- Estatísticas ");
+        printf("\n 4- Mostrar Memória Principal");
+        printf("\n 5- Mostrar Memória Cache");
+        printf("\n 0. Sair");
+        printf("\n\n Entre a sua opcao: ");
+        scanf("%d", &op);
+        fflush(stdin);
+        switch (op)
+        {
+				case 0:
+            exit(0);
+        case 1:
+						lerMemoria(Fila, Bloco, MemPrincipal);
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            mostraMemoriaPricipal(MemPrincipal);
+            break;
+        case 5:
+            mostraMemoriaCache(MemCache);
+            break;
+        default:
+            printf("\nOpcao incorreta");
+            getchar();
+            getchar();
+            break;
+        }
+    }
+    getchar();
+    return 0;
+}
